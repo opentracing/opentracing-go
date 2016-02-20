e02c0aab7bf9a32f30a903ee34ce01d5875419ed
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-02-20 18:15:13 -0500

    Delete `standardtracer`
    it has been extracted to the `basictracer-go` repository.

--- standardtracer/recorder.go
+++
@@ -1,9 +0,0 @@
-package standardtracer
-
-// A SpanRecorder handles all of the `RawSpan` data generated via an
-// associated `Tracer` (see `NewStandardTracer`) instance. It also names
-// the containing process and provides access to a straightforward tag map.
-type SpanRecorder interface {
-	// Implementations must determine whether and where to store `span`.
-	RecordSpan(span RawSpan)
-}
