0a076005917e7c1c54bca17ebc09c42e173d7657
Yuri Shkuro <ys@uber.com>
2016-02-26 20:28:49 -0500

    Change common tags to use Set instead of Add method. Add Span Kind.

--- ext/tags.go
+++ ext/tags.go
@@ -23,25 +23,54 @@ var (
 
 	// SamplingPriority determines the priority of sampling this Span.
 	SamplingPriority = uint16Tag("sampling.priority")
+
+	// SpanKind hints at relationship between spans, e.g. client/server
+	SpanKind = spanKindTag("span.kind")
+
+	// SpanKindRPCClient marks a span representing the client-side of an RPC
+	// or other remote call
+	SpanKindRPCClient = SpanKindEnum("c")
+
+	// SpanKindRPCServer marks a span representing the server-side of an RPC
+	// or other remote call
+	SpanKindRPCServer = SpanKindEnum("s")
 )
+
+// ---
+
+// SpanKindEnum represents common span types
+type SpanKindEnum string
+
+type spanKindTag string
+
+// Add adds a string tag to the `span`
+func (tag spanKindTag) Set(span opentracing.Span, value SpanKindEnum) {
+	span.SetTag(string(tag), value)
+}
+
+// ---
 
 type stringTag string
 
 // Add adds a string tag to the `span`
-func (tag stringTag) Add(span opentracing.Span, value string) {
+func (tag stringTag) Set(span opentracing.Span, value string) {
 	span.SetTag(string(tag), value)
 }
+
+// ---
 
 type uint32Tag string
 
 // Add adds a uint32 tag to the `span`
-func (tag uint32Tag) Add(span opentracing.Span, value uint32) {
+func (tag uint32Tag) Set(span opentracing.Span, value uint32) {
 	span.SetTag(string(tag), value)
 }
+
+// ---
 
 type uint16Tag string
 
 // Add adds a uint16 tag to the `span`
-func (tag uint16Tag) Add(span opentracing.Span, value uint16) {
+func (tag uint16Tag) Set(span opentracing.Span, value uint16) {
 	span.SetTag(string(tag), value)
 }
