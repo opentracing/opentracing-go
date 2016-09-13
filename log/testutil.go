5c9aebf9adbec5936a5d3bc433648eed55de9be0
Ben Sigelman <bhs@lightstep.com>
2016-09-13 11:47:39 -0700

    Fix lint stuttering

--- log/testutil.go
+++ log/testutil.go
@@ -6,21 +6,21 @@ import (
 	"testing"
 )
 
-// LogFieldValidator facilitates testing of Span.Log*() implementations.
+// FieldValidator facilitates testing of Span.Log*() implementations.
 //
-// LogFieldValidator SHOULD ONLY BE USED FOR TESTS. It is not intended for
+// FieldValidator SHOULD ONLY BE USED FOR TESTS. It is not intended for
 // production code.
 //
 // Usage:
 //
-//     lfv := log.NewLogFieldValidator(t, someLogStructure.Fields)
-//     lfv.
+//     fv := log.NewFieldValidator(t, someLogStructure.Fields)
+//     fv.
 //         ExpectNextFieldEquals("key1", reflect.String, "some string value").
-//         ExpectNextFieldEquals("key2", reflect.Uint8, "255")
+//         ExpectNextFieldEquals("key2", reflect.Uint32, "4294967295")
 //
-// LogFieldValidator satisfies the log.Encoder interface and thus is able to
+// FieldValidator satisfies the log.Encoder interface and thus is able to
 // marshal log.Field instances (which it takes advantage of internally).
-type LogFieldValidator struct {
+type FieldValidator struct {
 	t               *testing.T
 	fieldIdx        int
 	fields          []Field
@@ -29,10 +29,10 @@ type LogFieldValidator struct {
 	nextValAsString string
 }
 
-// NewLogFieldValidator returns a new validator that will test the contents of
+// NewFieldValidator returns a new validator that will test the contents of
 // `fields`.
-func NewLogFieldValidator(t *testing.T, fields []Field) *LogFieldValidator {
-	return &LogFieldValidator{
+func NewFieldValidator(t *testing.T, fields []Field) *FieldValidator {
+	return &FieldValidator{
 		t:      t,
 		fields: fields,
 	}
@@ -40,83 +40,83 @@ func NewLogFieldValidator(t *testing.T, fields []Field) *LogFieldValidator {
 
 // ExpectNextFieldEquals facilitates a fluent way of testing the contents
 // []Field slices.
-func (lfv *LogFieldValidator) ExpectNextFieldEquals(key string, kind reflect.Kind, valAsString string) *LogFieldValidator {
-	if len(lfv.fields) < lfv.fieldIdx {
-		lfv.t.Errorf("Expecting more than the %v Fields we have", len(lfv.fields))
+func (fv *FieldValidator) ExpectNextFieldEquals(key string, kind reflect.Kind, valAsString string) *FieldValidator {
+	if len(fv.fields) < fv.fieldIdx {
+		fv.t.Errorf("Expecting more than the %v Fields we have", len(fv.fields))
 	}
-	lfv.nextKey = key
-	lfv.nextKind = kind
-	lfv.nextValAsString = valAsString
-	lfv.fields[lfv.fieldIdx].Marshal(lfv)
-	lfv.fieldIdx++
-	return lfv
+	fv.nextKey = key
+	fv.nextKind = kind
+	fv.nextValAsString = valAsString
+	fv.fields[fv.fieldIdx].Marshal(fv)
+	fv.fieldIdx++
+	return fv
 }
 
 // EmitString satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitString(key, value string) {
-	lfv.validateNextField(key, reflect.String, value)
+func (fv *FieldValidator) EmitString(key, value string) {
+	fv.validateNextField(key, reflect.String, value)
 }
 
 // EmitBool satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitBool(key string, value bool) {
-	lfv.validateNextField(key, reflect.Bool, value)
+func (fv *FieldValidator) EmitBool(key string, value bool) {
+	fv.validateNextField(key, reflect.Bool, value)
 }
 
 // EmitInt satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitInt(key string, value int) {
-	lfv.validateNextField(key, reflect.Int, value)
+func (fv *FieldValidator) EmitInt(key string, value int) {
+	fv.validateNextField(key, reflect.Int, value)
 }
 
 // EmitInt32 satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitInt32(key string, value int32) {
-	lfv.validateNextField(key, reflect.Int32, value)
+func (fv *FieldValidator) EmitInt32(key string, value int32) {
+	fv.validateNextField(key, reflect.Int32, value)
 }
 
 // EmitInt64 satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitInt64(key string, value int64) {
-	lfv.validateNextField(key, reflect.Int64, value)
+func (fv *FieldValidator) EmitInt64(key string, value int64) {
+	fv.validateNextField(key, reflect.Int64, value)
 }
 
 // EmitUint32 satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitUint32(key string, value uint32) {
-	lfv.validateNextField(key, reflect.Uint32, value)
+func (fv *FieldValidator) EmitUint32(key string, value uint32) {
+	fv.validateNextField(key, reflect.Uint32, value)
 }
 
 // EmitUint64 satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitUint64(key string, value uint64) {
-	lfv.validateNextField(key, reflect.Uint64, value)
+func (fv *FieldValidator) EmitUint64(key string, value uint64) {
+	fv.validateNextField(key, reflect.Uint64, value)
 }
 
 // EmitFloat32 satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitFloat32(key string, value float32) {
-	lfv.validateNextField(key, reflect.Float32, value)
+func (fv *FieldValidator) EmitFloat32(key string, value float32) {
+	fv.validateNextField(key, reflect.Float32, value)
 }
 
 // EmitFloat64 satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitFloat64(key string, value float64) {
-	lfv.validateNextField(key, reflect.Float64, value)
+func (fv *FieldValidator) EmitFloat64(key string, value float64) {
+	fv.validateNextField(key, reflect.Float64, value)
 }
 
 // EmitObject satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitObject(key string, value interface{}) {
-	lfv.validateNextField(key, reflect.Interface, value)
+func (fv *FieldValidator) EmitObject(key string, value interface{}) {
+	fv.validateNextField(key, reflect.Interface, value)
 }
 
 // EmitLazyLogger satisfies the Encoder interface
-func (lfv *LogFieldValidator) EmitLazyLogger(key string, value LazyLogger) {
-	lfv.t.Error("Test infrastructure does not support EmitLazyLogger yet")
+func (fv *FieldValidator) EmitLazyLogger(key string, value LazyLogger) {
+	fv.t.Error("Test infrastructure does not support EmitLazyLogger yet")
 }
 
-func (lfv *LogFieldValidator) validateNextField(key string, actualKind reflect.Kind, value interface{}) {
-	if lfv.nextKey != key {
-		lfv.t.Errorf("Bad key: expected %q, found %q", lfv.nextKey, key)
+func (fv *FieldValidator) validateNextField(key string, actualKind reflect.Kind, value interface{}) {
+	if fv.nextKey != key {
+		fv.t.Errorf("Bad key: expected %q, found %q", fv.nextKey, key)
 	}
-	if lfv.nextKind != actualKind {
-		lfv.t.Errorf("Bad reflect.Kind: expected %v, found %v", lfv.nextKind, actualKind)
+	if fv.nextKind != actualKind {
+		fv.t.Errorf("Bad reflect.Kind: expected %v, found %v", fv.nextKind, actualKind)
 		return
 	}
-	if lfv.nextValAsString != fmt.Sprint(value) {
-		lfv.t.Errorf("Bad value: expected %q, found %q", lfv.nextValAsString, fmt.Sprint(value))
+	if fv.nextValAsString != fmt.Sprint(value) {
+		fv.t.Errorf("Bad value: expected %q, found %q", fv.nextValAsString, fmt.Sprint(value))
 	}
 	// All good.
 }
